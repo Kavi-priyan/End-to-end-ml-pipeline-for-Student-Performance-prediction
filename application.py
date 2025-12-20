7dd4e5f80767b8d904af7270fb0c3ce2fd2c9e50
Kavipriyan <131338635+Kavi-priyan@users.noreply.github.com>
2025-12-20 16:07:03 +0530

    temp fix

--- application.py
+++ application.py
@@ -72,5 +72,5 @@ def predict_datapoint():
 
 # Dynamic port for Render
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+   
+    app.run(host="0.0.0.0") 
